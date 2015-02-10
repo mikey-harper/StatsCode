7035bf2230178f1988ce9a6621d577f9966183dd
Ben Anderson <b.anderson@soton.ac.uk>
2015-02-10 13:51:08 +0000

    updated snippets files

--- r-snippets.R
+++ r-snippets.R
@@ -1,1 +1,9 @@
 * R snippets go here
+* Source of code acknowledged where known
+
+* ##Terms of Use
+* GPL: V2 - http://choosealicense.com/licenses/gpl-2.0/
+
+* See license file for details.
+
+* [YMMV](http://en.wiktionary.org/wiki/YMMV)

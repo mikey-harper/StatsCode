3bf4831d81e74d15d9be9d186b9240a6a09fd0ee
Ben Anderson <b.anderson@soton.ac.uk>
2015-02-11 14:45:17 +0000

    updated license etc

--- r-snippets.R
+++ r-snippets.R
@@ -1,13 +1,20 @@
-* R snippets go here
-* Source of code acknowledged where known
+# R snippets go here
+# Source of code acknowledged where known
 
-* ##Terms of Use
-* GPL: V2 - http://choosealicense.com/licenses/gpl-2.0/
+## Terms of Use ####
+# GPL: V2 - http://choosealicense.com/licenses/gpl-2.0/
 
-* See license file for details.
+# See license file for details.
 
-* [YMMV](http://en.wiktionary.org/wiki/YMMV)
+# [YMMV](http://en.wiktionary.org/wiki/YMMV)
 
-str(dataframe) * will produce a useful object summary of the frame
+# Datasets ####
+# R comes with a lot of built in datasets to play with see:
+library(help = "datasets")
 
-* you can use the data = option to specify a frame more easily
+# Summary functions ####
+summary(cars)
+* str(dataframe)  will produce a useful object summary of the frame
+str(cars)
+
+# Reshaping & manipulating data ####

1202fc99371db2518952807578cc08406b6d46a1
Ben Anderson <dataknut@icloud.com>
2016-05-23 14:38:33 +0100

    Added code for months & seasons

--- dateTimeSnippets.R
+++ dateTimeSnippets.R
@@ -12,3 +12,41 @@ DT$r_dow <- factor(DT$r_dow,
                      "Saturday"
                    )
 )
+
+
+# add  months
+DT$r_month <- as.POSIXlt(DT$r_epstart)$mon # 0 = January
+
+DT$r_month <- factor(DT$r_month,
+                     labels = c(
+                       "January", # 0
+                       "February",
+                       "March",
+                       "April",
+                       "May",
+                       "June",
+                       "July",
+                       "August",
+                       "September",
+                       "October",
+                       "November",
+                       "December" # 11 !
+                     )
+)
+
+# add seasons
+
+DT$season <- ifelse(DT$r_month > 1 & DT$r_month < 5, # Mar,Apr,May
+                    "Spring", NA
+)
+DT$season <- ifelse(DT$r_month > 4 & DT$r_month < 8, # Jun,Jul,Aug
+                    "Summer", DT$season
+)
+DT$season <- ifelse(DT$r_month > 7 & DT$r_month < 11, # Sep, Oct, Nov
+                    "Autumn", DT$season
+)
+DT$season <- ifelse(DT$r_month == 11 | # Dec, Jan, Feb
+                      DT$r_month == 0 | 
+                      DT$r_month == 1 ,
+                    "Winter", DT$season
+)

8a2fe7f9a46d2a9be3c91cd839a7f26b16820732
Ben Anderson <dataknut@icloud.com>
2016-09-26 14:43:48 +0100

    looping over names to do something

--- loopingOverVarNamesExample.R
+++ loopingOverVarNamesExample.R
@@ -10,3 +10,29 @@ for(i in vars)
                                           1, # at least 1 episode
                                           0)
 }
+
+files = c(Clamp = "wmgClampDT", SmartPlug = "wmgSmartPlugDT")
+
+for(i in names(files))
+{
+  ofile <- paste0(odpath, "all", i ,"sProcessed_updated_",
+                  Sys.Date(),
+                  ".csv")
+  print(paste0("Saving processed DT to: ", ofile, ".")
+  )
+  print("Variables saved:")
+  print(
+    names(
+      eval(
+        parse(text = files[[i]]) # surely has to be an easier way than this?!
+      )
+    )
+  )
+  
+  write.csv(eval(parse(text = files[[i]])), # condensed version of the above
+            file = ofile, row.names = FALSE
+  )
+  print("Now gzip that file")
+  cmd <- paste0("gzip -f ", ofile," &")
+  system(cmd) # gzip & force over-write, shame can't do this directly as part of write
+}

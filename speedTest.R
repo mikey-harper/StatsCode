67ca426c58e040d78055bb565c2f609626fb88c6
Ben Anderson <dataknut@icloud.com>
2017-01-13 18:59:38 +0000

    reduced df size again

--- speedTest.R
+++ speedTest.R
@@ -1,10 +1,10 @@
 # Borrowed from https://www.r-bloggers.com/strategies-to-speedup-r-code/
 
 # Create the data frame
-col1 <- runif (12^4.5, 0, 2)
-col2 <- rnorm (12^4.5, 0, 2)
-col3 <- rpois (12^4.5, 3)
-col4 <- rchisq (12^4.5, 2)
+col1 <- runif (12^4, 0, 2)
+col2 <- rnorm (12^4, 0, 2)
+col3 <- rpois (12^4, 3)
+col4 <- rchisq (12^4, 2)
 df <- data.frame (col1, col2, col3, col4)
 
 # Original R code: Before vectorization and pre-allocation
